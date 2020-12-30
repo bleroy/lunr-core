8af5d06b7fc901232e140fd21314c6a5da4e23c9
Daniel Crenna <daniel.crenna@gmail.com>
2020-12-30 14:57:35 -0500

    fix collection definition name

--- LunrCoreLmdbTests/TempDirectoryCollection.cs
+++ LunrCoreLmdbTests/TempDirectoryCollection.cs
@@ -1,10 +1,7 @@
-// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
 using Xunit;
 
 namespace LunrCoreLmdbTests
 {
-    [CollectionDefinition(nameof(TempDirectoryCollection))]
+    [CollectionDefinition(nameof(TempDirectory))]
     public class TempDirectoryCollection : ICollectionFixture<TempDirectory> { }
 }
