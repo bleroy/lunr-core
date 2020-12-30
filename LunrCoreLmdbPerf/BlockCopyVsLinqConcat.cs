6f899eaa496787b429e265aaab6cec02aed00ecb
Daniel Crenna <daniel.crenna@gmail.com>
2020-12-30 12:17:17 -0500

    add benchmark for BlockCopy

---
+++ LunrCoreLmdbPerf/BlockCopyVsLinqConcat.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace LunrCoreLmdbPerf
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    public class BlockCopyVsLinqConcat
+    {
+        private byte[] _left;
+        private byte[] _right;
+
+        [GlobalSetup]
+        public void GlobalSetUp()
+        {
+            _left = Encoding.UTF8.GetBytes("T:");
+            _right = Encoding.UTF8.GetBytes("word");
+        }
+
+        [Benchmark]
+        public void BlockCopy()
+        {
+            var buffer = new byte[_left.Length + _right.Length];
+            Buffer.BlockCopy(_left, 0, buffer, 0, _left.Length);
+            Buffer.BlockCopy(_right, 0, buffer, _left.Length, _right.Length);
+        }
+
+        [Benchmark]
+        public void LinqConcat()
+        {
+            var buffer = _left.Concat(_right).ToArray();
+        }
+    }
+}
