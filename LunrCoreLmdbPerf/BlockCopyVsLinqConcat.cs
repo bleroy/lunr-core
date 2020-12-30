3428dae7c74d94ef5168fd076151b0f1af2789c7
Daniel Crenna <daniel.crenna@gmail.com>
2020-12-30 12:17:40 -0500

    remote faulty attrib

--- LunrCoreLmdbPerf/BlockCopyVsLinqConcat.cs
+++ LunrCoreLmdbPerf/BlockCopyVsLinqConcat.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using System;
+﻿using System;
 using System.Linq;
 using System.Text;
 using BenchmarkDotNet.Attributes;
