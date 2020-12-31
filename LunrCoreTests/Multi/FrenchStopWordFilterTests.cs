22d8cd6efb8ce0c902d492007fb95677c9737583
Daniel Crenna <daniel.crenna@gmail.com>
2020-12-31 13:48:55 -0500

    add french stop word filter and tests

---
+++ LunrCoreTests/Multi/FrenchStopWordFilterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Lunr;
+using Lunr.Multi;
+using Xunit;
+
+namespace LunrCoreTests.Multi
+{
+    public class FrenchStopWordFilterTests
+    {
+        private readonly StopWordFilterBase filter = new FrenchStopWordFilter();
+
+        [Fact]
+        public async Task StopWordFilterFiltersStopWords()
+        {
+            string[] stopWords = new[] { "aurai", "elle", "leurs", "soyons", "êtes" };
+            
+            foreach (string word in stopWords)
+            {
+                Assert.True(filter.IsStopWord(word));
+                Assert.Empty(await filter.FilterFunction.BasicallyRun(word));
+            }
+        }
+
+        [Fact]
+        public async Task StopWordFilterIgnoresNonStopWords()
+        {
+            string[] nonStopWords = new[] { "baleine", "bisou", "brindille", "câlin" };
+
+            foreach (string word in nonStopWords)
+            {
+                Assert.False(filter.IsStopWord(word));
+                Assert.Equal(new[] { word }, await filter.FilterFunction.BasicallyRun(word));
+            }
+        }
+    }
+}
