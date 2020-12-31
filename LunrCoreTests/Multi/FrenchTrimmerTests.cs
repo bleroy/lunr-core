993846793704a837f51b14f2838b206afddc6f1d
Daniel Crenna <daniel.crenna@gmail.com>
2020-12-31 13:58:16 -0500

    add FrenchTrimmer and tests

---
+++ LunrCoreTests/Multi/FrenchTrimmerTests.cs
@@ -0,0 +1,21 @@
+﻿using Lunr.Multi;
+using Xunit;
+
+namespace LunrCoreTests.Multi
+{
+    public class FrenchTrimmerTests
+    {
+        [Theory]
+        [InlineData("d’Amritsar", "d’Amritsar")] // word
+        [InlineData("français.", "français")] // full stop
+        [InlineData("l'accès", "l'accès")] // inner apostrophe
+        [InlineData("Chloë'", "Chloë")] // trailing apostrophe
+        [InlineData("C’est!'", "C’est")] // exclamation mark
+        [InlineData("L’âge,'", "L’âge")] // comma
+        [InlineData("[nationalité]'", "nationalité")] // brackets
+        public void CheckTrim(string str, string expected)
+        {
+            Assert.Equal(expected, new FrenchTrimmer().Trim(str));
+        }
+    }
+}
