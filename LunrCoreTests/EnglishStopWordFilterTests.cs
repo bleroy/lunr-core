10936ecc9bbdc2e44e9b1e5b64c5ed9e20e58ce6
Dave Glick <dave@daveaglick.com>
2021-06-29 15:48:34 -0400

    Add an optional comparer to Set<T> and use it in the EnglishStopWordFilter for case-insensitive stop words

---
+++ LunrCoreTests/EnglishStopWordFilterTests.cs
@@ -0,0 +1,23 @@
+﻿using Lunr;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LunrCoreTests
+{
+    public class EnglishStopWordFilterTests
+    {
+        private readonly StopWordFilterBase filter = new EnglishStopWordFilter();
+
+        [Fact]
+        public async Task EnglishStopWordFilterIgnoresCase()
+        {
+            string[] stopWords = new[] { "the", "The", "THE" };
+            
+            foreach (string word in stopWords)
+            {
+                Assert.True(filter.IsStopWord(word));
+                Assert.Empty(await filter.FilterFunction.BasicallyRun(word));
+            }
+        }
+    }
+}
